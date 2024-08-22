3cbe9862c5fb3b486f1c982cbb4699558c496476
ahmed <ahmed@nda.com>
2024-08-22 17:51:37 +0300

    adding pipeline info

--- scripts/appCenterDeploy.sh
+++ scripts/appCenterDeploy.sh
@@ -6,7 +6,7 @@ APP_NAME="Qenawi/BuildExample"
 FILE_PATH="app/build/outputs/apk/debug/app-debug.apk"
 GROUP_NAME="Testers"  # Replace with your distribution group
 RELEASE_NOTES="Automated release via GitHub Actions"
-TOKEN="388bb7c47b6d49e780cf76f3967565b95c708df5"  # Replace with your actual token or use an environment variable
+TOKEN="$Token"  # Replace with your actual token or use an environment variable
 
 # Run the appcenter distribute command
 appcenter distribute release \
